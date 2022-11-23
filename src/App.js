ca7f14bc41db5389f4f61caf683a60a64a38604c
Abhishek Bang <68838873+abhishek1052@users.noreply.github.com>
2022-11-23 13:39:18 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Profilepic from "./components/Profilepic";
 import Username from "./components/Username";
 import Description from "./components/Description";
 import Repo from "./components/Repo";
-import { useEffect } from 'react';
+import { useEffect, } from 'react';
+import Error404 from "./components/Error404";
 import {
   Card,
   Table,
@@ -14,6 +15,7 @@ import {
   TablePagination
 } from '@mui/material';
 function App() {
+  const [error404, changeStatus] = React.useState(false);
   const [username, changeUsername] = React.useState("");
   const [isloading, loadingstatus] = React.useState(true);
   const [repos, changerepos] = React.useState([]);
@@ -45,12 +47,22 @@ function App() {
       var url = "https://api.github.com/users/" + username + "/repos?page=" + page + "&per_page=" + controller.rowsPerPage;
       if (username !== "") {
         try {
-          fetch(url)
-            .then((response) => response.json())
+          fetch(url, {
+            headers: {
+              'Authorization': 'token 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',
+            }
+          })
+            .then(response => {
+              if (response.status === 404) {
+                return Promise.reject('error 404');
+              }
+              else {
+                changeStatus(false)
+                return response.json()
+              }
+            })
             .then(
-
               function (data) {
-
                 var array = []
                 for (var i = 0; i < data.length; i++) {
                   array.push({
@@ -62,8 +74,11 @@ function App() {
                 changerepos(array);
               }
             )
+            .catch(error => {
+              changeStatus(true)
+            })
         } catch (error) {
-          console.log(error);
+          console.log("error1");
         }
       }
     };
@@ -75,16 +90,27 @@ function App() {
       var url = "https://api.github.com/users/" + username;
       if (username !== "") {
         try {
-          fetch(url)
-            .then((response) => response.json())
+          fetch(url, {
+            headers: {
+              'Authorization': 'token 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',
+            }
+          })
+            .then(response => {
+              if (response.status === 404) {
+                return Promise.reject('error 404');
+              }
+              else {
+                changeStatus(false)
+                return response.json()
+              }
+            })
             .then(
-
               function (data) {
                 setRepoCount(data.public_repos)
               }
-            )
+            ).catch(error => { changeStatus(true) })
         } catch (error) {
-          console.log(error);
+          console.log("error1");
         }
       }
     };
@@ -99,44 +125,53 @@ function App() {
       {username !== "" ?
         <div>
 
-          {!isloading ? <div className="overallcontent">
-            <div className="user_info" >
-              <Profilepic username={username} />
-              <Description username={username} />
+          {!isloading ?
+
+            <div>
+              {error404 ?
+                <Error404 />
+                :
+                <div className="overallcontent">
+                  <div className="user_info" >
+                    <Profilepic username={username} />
+                    <Description username={username} />
+                  </div>
+                  <div className="allrepos" >
+
+                    <Card>
+                      <Table>
+                        <TableHead>
+
+                        </TableHead>
+                        <TableBody>
+                          {
+                            repos.map((e, index) => {
+
+                              return <TableRow key={index}>
+                                <TableCell key={index}>
+                                  <Repo key={index} name={e.name} description={e.description} user={username} />
+                                </TableCell>
+                              </TableRow>
+                            })
+
+                          }
+
+                        </TableBody>
+                      </Table>
+                      <TablePagination
+                        component="div"
+                        onPageChange={handlePageChange}
+                        page={controller.page}
+                        count={repoCount}
+                        rowsPerPage={controller.rowsPerPage}
+                        onRowsPerPageChange={handleChangeRowsPerPage}
+                      />
+                    </Card>
+
+                  </div>
+                </div>
+              }
             </div>
-            <div className="allrepos" >
-
-              <Card>
-                <Table>
-                  <TableHead>
-
-                  </TableHead>
-                  <TableBody>
-                    {
-                      repos.map((e, index) => {
-                        return <TableRow key={index}>
-                          <TableCell key={index}>
-                            <Repo key={index} name={e.name} description={e.description} user={username} />
-                          </TableCell>
-                        </TableRow>
-                      })
-
-                    }
-
-                  </TableBody>
-                </Table>
-                <TablePagination
-                  component="div"
-                  onPageChange={handlePageChange}
-                  page={controller.page}
-                  count={repoCount}
-                  rowsPerPage={controller.rowsPerPage}
-                  onRowsPerPageChange={handleChangeRowsPerPage}
-                />
-              </Card>
-
-            </div>
-          </div>
             : <div className="loader"></div>
           }
         </div>
